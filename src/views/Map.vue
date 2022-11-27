115b3422af566f7177e6203bd846296659e08432
xiaolongtuan <2765504026@qq.com>
2022-11-27 17:52:01 +0800

    map多点标记+文本描述

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -11,14 +11,12 @@ export default {
       addressText: null,
       geometries: [
         {
-          styleId: "marker",
           position: new TMap.LatLng(39.984104, 116.307503),
           properties: {
             content: "果洛新村黄河乡玛多县果洛藏族自治州青海省",
           },
         },
         {
-          styleId: "marker",
           position: new TMap.LatLng(39.984104, 116),
           properties: {
             content: "asdasdasf",
@@ -46,36 +44,46 @@ export default {
       this.map = new TMap.Map("container", {
         center: center,
       });
+      this.createMarker();
+    },
+    createMarker() {
+      if (this.marker == null) {
+        //初始marker
+        this.marker = new TMap.MultiMarker({
+          id: "marker-layer",
+          map: this.map,
+          styles: {
+            marker: new TMap.MarkerStyle({
+              width: 24,
+              height: 35,
+              anchor: { x: 12, y: 35 },
+              src: "https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/markerDefault.png",
+            }),
+          },
+          geometries: this.geometries,
+        });
 
-      //初始marker
-      this.marker = new TMap.MultiMarker({
-        id: "marker-layer",
-        map: this.map,
-        styles: {
-          marker: new TMap.MarkerStyle({
-            width: 24,
-            height: 35,
-            anchor: { x: 12, y: 35 },
-            src: "https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/markerDefault.png",
-          }),
-        },
-        geometries: this.geometries,
-      });
+        var infoWindow = new TMap.InfoWindow({
+          map: this.map,
+          position: new TMap.LatLng(39.984104, 116.307503),
+          offset: { x: 0, y: -32 }, //设置信息窗相对position偏移像素
+        });
 
-      var infoWindow = new TMap.InfoWindow({
-        map: this.map,
-        position: new TMap.LatLng(39.984104, 116.307503),
-        offset: { x: 0, y: -32 }, //设置信息窗相对position偏移像素
-      });
-
-      infoWindow.close(); //初始关闭信息窗关闭
-      //监听标注点击事件
-      this.marker.on("click", function (evt) {
-        //设置infoWindow
-        infoWindow.open(); //打开信息窗
-        infoWindow.setPosition(evt.geometry.position); //设置信息窗位置
-        infoWindow.setContent(evt.geometry.properties.content); //设置信息窗内容
-      });
+        infoWindow.close(); //初始关闭信息窗关闭
+        //监听标注点击事件
+        this.marker.on("click", function (evt) {
+          //设置infoWindow
+          infoWindow.open(); //打开信息窗
+          infoWindow.setPosition(evt.geometry.position); //设置信息窗位置
+          infoWindow.setContent(evt.geometry.properties.content); //设置信息窗内容
+        });
+      }
+    },
+    removeMarker() {
+      if (this.marker!=null) {
+        this.marker.setMap(null);
+        this.marker = null;
+      }
     },
 
     //地址解析
@@ -89,12 +97,14 @@ export default {
           servicesk: "vPGsh5j1tzygzWILvhNtWTMpsUi9VEha",
         })
         .then((result) => {
-          this.marker.geometries.push({
+          this.geometries.push({
             position: result.result.location,
             properties: {
               content: address,
             },
           });
+          this.removeMarker();
+          this.createMarker();
           theMap.setCenter(result.result.location);
         });
     },
