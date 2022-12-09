6996fd8ec030d00e5e551001dc641e3436da0a03
sea-tortoise <118054249+sea-tortoise@users.noreply.github.com>
2022-12-09 23:31:45 +0800

    Update DataList.vue
    编码描述

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -8,6 +8,13 @@
       <el-table-column prop="disasterIndi" label="灾情指标"> </el-table-column>
       <el-table-column prop="origin" label="来源"> </el-table-column>
       <el-table-column prop="type" label="载体"> </el-table-column>
+       <el-table-column prop="text" label="描述">
+        　<template slot-scope="scope">
+        　　　　　　　　　　<span v-if="scope.row.type=== '文字'">{{scope.row.text}}</span>
+        　　　　　　　　　　<span v-if="scope.row.type=== '图像'"><a href="" :download="'http://10.128.236.33:8080/'+scope.row.media">点击下载图片</a></span>
+                        　<span v-if="scope.row.type=== '视频'"><a :href="'http://10.128.236.33:8080/'+scope.row.media">点击查看视频</a></span>
+        　　　　　　　　</template>
+      </el-table-column>
     </el-table>
     </el-card>
   </div>
